bc5ac7e433de11edd98e59102ecd3781bee91702
Vasiliy <vasiliy.trunov.00@mail.ru>
2022-08-09 22:50:21 +0200

    Задача №6

---
+++ program_006/Program.cs
@@ -0,0 +1,7 @@
+﻿// принимаем на вход число и выдаем True если оно четное, False нечетное
+
+System.Console.WriteLine("Введите число: ");
+int number = int.Parse(System.Console.ReadLine());
+
+if (number%2 == 0) System.Console.WriteLine(true);
+else System.Console.WriteLine(false);
