f0f787801d394a88039844e9cf6f4cbba9c94d6e
Vasiliy <vasiliy.trunov.00@mail.ru>
2022-08-09 22:48:20 +0200

    Задача №2

---
+++ program_002/Program.cs
@@ -0,0 +1,16 @@
+﻿// принимаем на вход 2 числа и выдаем наибольшее
+
+System.Console.WriteLine("Ведите первое число: ");
+int n_1 = int.Parse(System.Console.ReadLine());
+System.Console.WriteLine("Ведите второе число: ");
+int n_2 = int.Parse(System.Console.ReadLine());
+
+if (n_1 < n_2 || n_1 != n_2){
+    System.Console.WriteLine($"Наибольшее число - {n_2}");
+}
+else if (n_1 > n_2 || n_1 != n_2) {
+    System.Console.WriteLine($"Наибольшее число - {n_1}");
+}
+else {
+    System.Console.WriteLine("Они равны");
+}
