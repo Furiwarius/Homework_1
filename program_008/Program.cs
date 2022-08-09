f65c2c6fdd660fa29b7c13c0c9526a63613b18ed
Vasiliy <vasiliy.trunov.00@mail.ru>
2022-08-09 22:51:28 +0200

    Задача №8

---
+++ program_008/Program.cs
@@ -0,0 +1,17 @@
+﻿// принимаем на вход число и выдаем все четные от 0 до выданного числа
+
+System.Console.WriteLine("Введите число: ");
+int number = int.Parse(System.Console.ReadLine());
+int i = 0;
+string rt = "";
+while (i<= number)
+{
+    if (i%2==0) {
+        if (i==0) {i++; continue;}
+        else if (rt=="") rt+=i;
+        else rt+=", " + i;
+    }
+    i++;
+    
+}
+System.Console.WriteLine(rt);
