38bf57be4d0450b6e9a03b2ca3ffa07c402fdf5a
Vasiliy <vasiliy.trunov.00@mail.ru>
2022-08-11 22:02:08 +0200

    Убрал лишнее условие

--- program_002/Program.cs
+++ program_002/Program.cs
@@ -5,10 +5,10 @@ int n_1 = int.Parse(System.Console.ReadLine());
 System.Console.WriteLine("Ведите второе число: ");
 int n_2 = int.Parse(System.Console.ReadLine());
 
-if (n_1 < n_2 || n_1 != n_2){
+if (n_1 < n_2){
     System.Console.WriteLine($"Наибольшее число - {n_2}");
 }
-else if (n_1 > n_2 || n_1 != n_2) {
+else if (n_1 > n_2) {
     System.Console.WriteLine($"Наибольшее число - {n_1}");
 }
 else {
